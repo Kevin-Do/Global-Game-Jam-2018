4c93f7061d4a41a80aee2b1521e1ca15c11cff55
Kevin Do <kevindokhoale@gmail.com>
2018-01-26 22:12:49 -0800

    Project Setup
    + Player
    + Game Manager
    + Menus In Progress

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+
+	public static GameManager Instance = null;
+	public GameObject Player;
+
+	private void Awake()
+	{
+		if (Instance == null)
+		{
+			Instance = this;
+		}
+		else if (Instance != this)
+		{
+			Destroy(gameObject);
+		}
+		DontDestroyOnLoad(gameObject);
+	}
+
+	// Update is called once per frame
+	void Update () {
+		PauseGame();
+	}
+
+	void PauseGame()
+	{
+		if (Input.GetKey(KeyCode.Escape))
+		{
+			var paused = Player.GetComponent<PlayerController>().freezePlayer;
+			Player.GetComponent<PlayerController>().freezePlayer = !paused;
+		}
+	}
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+	//Components
+	public Rigidbody2D rb;
+	
+	//Player Factors
+	public float playerSpeed;
+	public float playerJumpForce;
+	private bool canJump;
+	public bool freezePlayer;
+	
+	//Jump Variables
+	public int jumpCount = 0;
+	public int jumpLimit = 1;
+
+	void Awake () {
+		rb = GetComponent<Rigidbody2D>();
+	}
+	
+	
+	void Update() {
+		
+		//Handle Pause Menu
+		if (freezePlayer)
+		{
+			return;
+		}
+		
+		//Handle Jumping
+		Jump();
+
+		//Handle Movement
+		Move();
+	}
+
+	void Move() 
+	{
+		float moveHorizontal = Input.GetAxisRaw("Horizontal");
+		rb.velocity = new Vector2(moveHorizontal * playerSpeed, rb.velocity.y);
+
+		//Handle facing left/right
+		if (moveHorizontal != 0f && (moveHorizontal < 0f) != transform.localScale.x < 0) 
+		{
+			Flip();
+		}
+	}
+	
+	void Flip() 
+	{
+		Vector3 currentScale = transform.localScale;
+		currentScale.x *= -1;
+		transform.localScale = currentScale;
+	}
+
+	void Jump()
+	{
+		if (Input.GetButtonDown("Jump") && jumpCount < jumpLimit && canJump)
+		{
+			jumpCount++;
+			Vector2 movement = Vector2.up * playerJumpForce;
+			rb.velocity = new Vector2(rb.velocity.x, (float) 0.1);
+			rb.AddForce(movement * playerSpeed);
+		}
+	}
+
+	void OnCollisionStay2D(Collision2D col) {
+		if (col.gameObject.tag == "Floor") {
+			canJump = true;
+			jumpCount = 0;
+		}
+	}
+}
